f44f881fe1728b091ef3804dbeddf8851ad977a6
Xiao215 <xiaozhang20030215@gmail.com>
2024-03-11 20:23:46 -0400

    Convert most of the code to just pytorch. Update the data pipeline.

--- teach_cs.sh
+++ teach_cs.sh
@@ -1,7 +1,7 @@
 # Run the following command to submit the job to the cluster
-srun -p csc401 --gres gpu python3 -u train.py --wandb
+srun -p csc413 --gres gpu python3 -u train.py --wandb
 
 # Check how many people using gpu
-squeue -p csc401
+squeue -p csc413
 
-srun -p csc401 --gres gpu python3 -u fastai_script.py --wandb
+srun -p csc413 --gres gpu python3 -u fastai_script.py --wandb
