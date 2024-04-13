c409a9abfa07b2fcf876017d2e36786a545467ae
Xiao215 <xiaozhang20030215@gmail.com>
2024-04-13 11:19:14 -0400

    fix pipeline issue

--- fastai_script.py
+++ fastai_script.py
@@ -218,47 +218,6 @@ if args.wandb:
 #         for batch in self.dataloader:
 #             yield tuple(dict_to(x, self.device) for x in batch)
 
-class SinusoidalPosEmb(nn.Module):
-    def __init__(self, dim=16, M=10000):
-        super().__init__()
-        self.dim = dim
-        self.M = M
-
-    def forward(self, x):
-        device = x.device
-        half_dim = self.dim // 2
-        emb = math.log(self.M) / half_dim
-        emb = torch.exp(torch.arange(half_dim, device=device) * (-emb))
-        emb = x[...,None] * emb[None,...]
-        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
-        return emb
-
-class RNA_Model(nn.Module):
-    def __init__(self, dim=192, depth=12, head_size=32, **kwargs):
-        super().__init__()
-        self.emb = nn.Embedding(4,dim)
-        self.pos_enc = SinusoidalPosEmb(dim)
-        self.transformer = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model=dim, nhead=dim//head_size, dim_feedforward=4*dim,
-                dropout=0.1, activation=nn.GELU(), batch_first=True, norm_first=True), depth)
-        self.proj_out = nn.Linear(dim,2)
-
-    def forward(self, x0):
-        mask = x0['mask']
-        Lmax = mask.sum(-1).max()
-        mask = mask[:,:Lmax]
-        x = x0['seq'][:,:Lmax]
-
-        pos = torch.arange(Lmax, device=x.device).unsqueeze(0)
-        pos = self.pos_enc(pos)
-        x = self.emb(x)
-        x = x + pos
-
-        x = self.transformer(x, src_key_padding_mask=~mask)
-        x = self.proj_out(x)
-
-        return x
-
 def loss(pred,target):
     p = pred[target['mask'][:,:pred.shape[1]]]
     y = target['react'][target['mask']].clip(0,1)
